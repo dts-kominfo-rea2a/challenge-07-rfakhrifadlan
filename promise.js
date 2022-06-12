e765fc6f1b116578470a042c4d758e6b4594919a
rfakhrifadlan <rfakhrifadlan@gmail.com>
2022-06-12 21:21:57 +0700

    done

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (esmosi) => {
+  let ixx = await promiseTheaterIXX();
+  let vgc = await promiseTheaterVGC();
+  let allMovie = [...ixx, ...vgc];
+
+  let totalEmosi = 0;
+  allMovie.map((e) => (e.hasil == esmosi ? (totalEmosi += 1) : null));
+
+  return totalEmosi;
+};
 
 module.exports = {
   promiseOutput,
